07d2ee9503c8988f57cd253d400d8489832759e9
JordanH <jordanh@apadmi.com>
2023-10-25 19:56:02 +0100

    Convert to lexer iterator

--- src/lexer.rs
+++ src/lexer.rs
@@ -3,9 +3,7 @@ use std::collections::HashMap;
 // Tokens in the basic hello world application ()
 #[derive(Debug, PartialEq, Clone)]
 enum Token {
-    //fun
-    Fun,
-    //Anything that isn't a designated token
+    // Anything that isn't a designated token
     StringSymbol { symbol: String }, //e.g. fun [main]() or "[Hello],[World]"
     // ()
     LeftBracket,
@@ -14,7 +12,6 @@ enum Token {
     LeftSquirly,
     RightSquirly,
     // println
-    Println,
     // " || '
     Quote,
     // ,
@@ -22,123 +19,158 @@ enum Token {
     Space,
 }
 
-fn tokenize(input: &str) -> Result<Vec<Token>, ()> {
-    let map: HashMap<String, Token> = HashMap::from([
-        (String::from("fun"), Token::Fun),
-        (String::from("("), Token::LeftBracket),
-        (String::from(")"), Token::RightBracket),
-        (String::from("{"), Token::LeftSquirly),
-        (String::from("}"), Token::RightSquirly),
-        (String::from("println"), Token::Println),
-        (String::from("\""), Token::Quote),
-        (String::from(","), Token::Comma),
-        (String::from(" "), Token::Space),
-    ]);
-    //Remove spaces
-    let tokens: Vec<Token> = input
-        .chars()
-        .map(|v| {
-            let token = &map[&v.to_string()];
-            token.clone()
-        })
-        .collect::<Vec<Token>>();
-    //fun main() { } -> [fun],[main()],[{],[}]
-    //
-    //
-    //
-    //
+#[derive(Debug, Clone)]
+struct Lexer {
+    position: usize,
+    input: String,
+    character: Option<char>,
+}
+fn map_character(character: Option<&char>) -> Option<Token> {
+    match character {
+        Some(c) => {
+            let character_map: HashMap<char, Token> = HashMap::from([
+                ('(', Token::LeftBracket),
+                (')', Token::RightBracket),
+                ('{', Token::LeftSquirly),
+                ('}', Token::RightSquirly),
+                ('\"', Token::Quote),
+                ('\'', Token::Quote),
+                (',', Token::Comma),
+                (' ', Token::Space),
+            ]);
 
-    Ok(tokens)
+            character_map.get(c).cloned()
+        }
+        None => None,
+    }
+}
+// fn map_keyword(keyword: &String) -> Option<Token> {
+//     let keyword_map: HashMap<String, Token> = HashMap::from([
+//         (String::from("fun"), Token::Fun),
+//         (String::from("println"), Token::Println),
+//     ]);
+//     keyword_map.get(keyword).cloned()
+// }
+impl Lexer {
+    fn new(input: String) -> Lexer {
+        let character = input.chars().next();
+        let token = map_character(character.as_ref());
+        Lexer {
+            input,
+            position: 0,
+            character,
+        }
+    }
+
+    fn next_token(&self) -> (Lexer, Option<Token>) {
+        let character = self.input.chars().nth(self.position);
+        let token = map_character(character.as_ref());
+
+        (
+            Lexer {
+                position: self.position + 1,
+                input: self.input.to_string(),
+                character,
+            },
+            token,
+        )
+    }
 }
 
-// fun[ ]main - space delimited
-// ([]) - character delimited
-// Which means we need to handle keywords on the space delimit level + characters on chars level
+fn tokenize(input: &str) -> Result<Vec<Token>, ()> {
+    //fun main() { } -> [fun],[main()],[{],[}]
+
+    let result = Lexer::new(input.to_string()).next_token();
+    let lexer = result.0;
+    let token = result.1;
+
+    Ok(vec![])
+}
+
+// problem
+// we currently assess everything on the character level - but keywords are multi character
 
 #[cfg(test)]
 mod tests {
+    use crate::lexer::Lexer;
+
     use super::{tokenize, Token};
+
     #[test]
-    fn tokenize_with_fun_keyword() {
-        let expected = vec![Token::Fun];
+    fn space_input_success() {
+        let lexer = Lexer::new(String::from(" "));
+        let expected = Token::Space;
 
-        let actual = tokenize("fun").unwrap();
+        let actual = lexer.next_token().1.unwrap();
 
-        assert_eq!(expected[0], actual[0]);
-    }
-    #[test]
-    fn tokenize_with_lb_symbol() {
-        let expected = vec![Token::LeftBracket];
-
-        let actual = tokenize("(").unwrap();
-
-        assert_eq!(expected[0], actual[0]);
-    }
-    #[test]
-    fn tokenize_with_rb_symbol() {
-        let expected = vec![Token::RightBracket];
-
-        let actual = tokenize(")").unwrap();
-
-        assert_eq!(expected[0], actual[0]);
-    }
-    #[test]
-    fn tokenize_with_ls_symbol() {
-        let expected = vec![Token::LeftSquirly];
-
-        let actual = tokenize("{").unwrap();
-
-        assert_eq!(expected[0], actual[0]);
-    }
-    #[test]
-    fn tokenize_with_rs_symbol() {
-        let expected = vec![Token::RightSquirly];
-
-        let actual = tokenize("}").unwrap();
-
-        assert_eq!(expected[0], actual[0]);
+        assert_eq!(expected, actual);
     }
 
     #[test]
-    fn tokenize_with_println_symbol() {
-        let expected = vec![Token::Println];
+    fn lb_input_success() {
+        let lexer = Lexer::new(String::from("("));
+        let expected = Token::LeftBracket;
 
-        let actual = tokenize("println").unwrap();
+        let actual = lexer.next_token().1.unwrap();
 
-        assert_eq!(expected[0], actual[0]);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn rb_input_success() {
+        let lexer = Lexer::new(String::from(")"));
+        let expected = Token::RightBracket;
+
+        let actual = lexer.next_token().1.unwrap();
+
+        assert_eq!(expected, actual);
     }
     #[test]
-    fn tokenize_with_quote_symbol() {
-        let expected = vec![Token::Quote];
+    fn ls_input_success() {
+        let lexer = Lexer::new(String::from("{"));
+        let expected = Token::LeftSquirly;
 
-        let actual = tokenize("\"").unwrap();
+        let actual = lexer.next_token().1.unwrap();
 
-        assert_eq!(expected[0], actual[0]);
+        assert_eq!(expected, actual);
     }
     #[test]
-    fn tokenize_with_comma_symbol() {
-        let expected = vec![Token::Comma];
+    fn rs_input_success() {
+        let lexer = Lexer::new(String::from("}"));
+        let expected = Token::RightSquirly;
 
-        let actual = tokenize(",").unwrap();
+        let actual = lexer.next_token().1.unwrap();
 
-        assert_eq!(expected[0], actual[0]);
+        assert_eq!(expected, actual);
     }
+
     #[test]
-    fn tokenize_with_lbrb_symbols() {
-        let expected = vec![Token::LeftBracket, Token::Space, Token::RightBracket];
+    fn comma_input_success() {
+        let lexer = Lexer::new(String::from(","));
+        let expected = Token::Comma;
 
-        let actual = tokenize("( )").unwrap();
+        let actual = lexer.next_token().1.unwrap();
 
-        assert_eq!(expected[0], actual[0]);
-        assert_eq!(expected[1], actual[1]);
+        assert_eq!(expected, actual);
     }
+
     #[test]
-    fn tokenize_with_lsrs_symbols() {
-        let expected = vec![Token::LeftSquirly, Token::Space, Token::RightSquirly];
+    fn quote_input_success() {
+        let lexer = Lexer::new(String::from("\'"));
+        let expected = Token::Quote;
 
-        let actual = tokenize("{ }").unwrap();
+        let actual = lexer.next_token().1.unwrap();
 
-        assert_eq!(expected[0], actual[0]);
-        assert_eq!(expected[1], actual[1]);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn double_quote_input_success() {
+        let lexer = Lexer::new(String::from("\""));
+        let expected = Token::Quote;
+
+        let actual = lexer.next_token().1.unwrap();
+
+        assert_eq!(expected, actual);
     }
 }
