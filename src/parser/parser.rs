a7344e254e8c09bd024df5d9ed02b4bc5e65320e
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2023-11-11 12:02:47 +0000

    Add node token to AstNode
    This is the value the ast node will contain

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,8 +1,9 @@
 use std::sync::Arc;
 
-use crate::shared::token::{Ident, Token};
+use crate::shared::token::Token;
 
 struct AstNode {
     parent: Box<Option<AstNode>>,
     children: Arc<Option<Vec<AstNode>>>,
+    node_token: Token,
 }
