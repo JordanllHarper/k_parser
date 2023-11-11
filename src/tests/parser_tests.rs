00c586ce7b471cdaa62dac6f14d0b1aa4e7980d5
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2023-11-11 21:05:09 +0000

    Add a test for the parser given a blank vector.
    Current implementation will default to a non identifiable given there is
    no tokens.

--- src/tests/parser_tests.rs
+++ src/tests/parser_tests.rs
@@ -1,7 +1,33 @@
 #[cfg(test)]
 mod parser_tests {
     // TODO: To test:
+    // - [ ] Make a single blank node ast
     // - [ ] Make a single node ast
     // - [ ] Make with 2 children
     // - [ ] Make with 3 children
+    use std::sync::Arc;
+
+    use crate::{
+        parser::{
+            ast_node::AstNode,
+            parser::{AstParser, Parser},
+        },
+        shared::token::Token,
+    };
+
+    #[test]
+    fn make_single_blank_node() {
+        let tokens: Vec<Token> = vec![];
+        let parser = AstParser::new(tokens);
+        let expected = AstNode::new(
+            Box::new(None),
+            Arc::new(None),
+            Token::Identifier(crate::shared::token::Ident::NonIdentifiable(String::from(
+                "",
+            ))),
+        );
+        let (node, _parser) = parser.update_tree();
+
+        assert_eq!(expected.node_token, node.node_token)
+    }
 }
