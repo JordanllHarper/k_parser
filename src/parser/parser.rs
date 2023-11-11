e22fd8139078be0abde541240fd2c07fdda3ae03
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2023-11-11 20:32:48 +0000

    Change some of the implementation details of the parser
    Some implementation details had to change with this commit.
    
    The tree is carried over in each new tree generation to follow the
    immutable pattern establised.
    This might be costly for performance.
    The new functions and advance functions needed to be adjusted because of
    this.

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,4 +1,6 @@
-use crate::shared::token::Token;
+use std::sync::Arc;
+
+use crate::shared::token::{Ident, Token};
 
 use super::ast_node::AstNode;
 
@@ -17,30 +19,50 @@ struct AstParser {
     tokens: Vec<Token>,
     current_token_position: usize,
     current_token: Option<Token>,
+    current_tree_root: AstNode,
 }
 
 impl AstParser {
     fn new(tokens: Vec<Token>) -> Self {
-        let start_token = tokens.get(0).cloned();
+        let start_token = tokens.get(0);
+
+        let start_node = match start_token {
+            Some(token) => AstNode::new(Box::new(None), Arc::new(None), token.clone()),
+            None => AstNode::new(
+                Box::new(None),
+                Arc::new(None),
+                Token::Identifier(Ident::NonIdentifiable(String::from(""))),
+            ),
+        };
         Self {
-            tokens,
+            tokens: tokens.clone(),
             current_token_position: 0,
-            current_token: start_token,
+            current_token: start_token.cloned(),
+            current_tree_root: start_node,
         }
     }
 
-    fn advance(&self) -> Self {
+    fn advance(&self, tree_root: AstNode) -> Self {
         let new_position = self.current_token_position + 1;
         let new_token = self.tokens.get(new_position);
         Self {
             tokens: self.tokens.clone(),
             current_token_position: new_position,
             current_token: new_token.cloned(),
+            current_tree_root: tree_root,
         }
     }
 }
 
 impl Parser for AstParser {
+    // bare minimum functionality
+    // One node -> e.g. ,
+    //
+    // Process:
+    // Read through tokens
+    // If current token is None, return the root with no operations
+    //
+
     fn update_tree(&self) -> (AstNode, Self) {
         todo!()
     }
