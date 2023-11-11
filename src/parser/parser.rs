781e71a0bfb1b81d48fe591e0904ad7036b72175
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2023-11-11 19:37:59 +0000

    Create a parser trait with documentation
    This trait will be the foundation for creating an AST.

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,7 +1,18 @@
 use crate::shared::token::Token;
 
-struct Parser {
+use super::ast_node::AstNode;
+
+trait Parser {
+    /// Advances the parser state to the next token.
+    fn advance(&self) -> Self;
+
+    /// Gets an updated tree root with the result of this Node.
+    ///
+    /// Will return the same tree if no more tokens are remaining.
+    fn next_node(&self) -> (AstNode, Self);
+}
+
+struct AstParser {
     tokens: Vec<Token>,
     current_token_position: u16,
 }
-
