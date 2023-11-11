7429b83bf3f6325d3b18d529d2f23afb6107c0cc
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2023-11-11 19:53:54 +0000

    Implement the advance method of the parser
    This required some changes to the AstParser structure.
    
    The index needs to use usize and the new function no longer needs a
    starting token as that can be figured out by the constructor.

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -14,24 +14,34 @@ trait Parser {
 
 /// The implementation struct for the parser.
 ///
-/// Contains the tokens of the building operation and the current position of the parser.
+/// Contains the tokens of the building operation, the current position of the parser
+/// and the current token being operated on.
 struct AstParser {
     tokens: Vec<Token>,
-    current_token_position: u16,
+    current_token_position: usize,
+    current_token: Option<Token>,
 }
 
 impl AstParser {
-    fn new(tokens: Vec<Token>, current_token_position: u16) -> Self {
+    fn new(tokens: Vec<Token>) -> Self {
+        let start_token = tokens.get(0).cloned();
         Self {
             tokens,
-            current_token_position,
+            current_token_position: 0,
+            current_token: start_token,
         }
     }
 }
 
 impl Parser for AstParser {
     fn advance(&self) -> Self {
-        todo!()
+        let new_position = self.current_token_position + 1;
+        let new_token = self.tokens.get(new_position);
+        Self {
+            tokens: self.tokens.clone(),
+            current_token_position: new_position,
+            current_token: new_token.cloned(),
+        }
     }
 
     fn next_node(&self) -> (AstNode, Self) {
