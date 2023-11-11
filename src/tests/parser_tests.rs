aba5657d2a59c5ede8512bb64069b9ebaa139edf
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2023-11-11 12:13:02 +0000

    Add testing guidance

--- src/tests/parser_tests.rs
+++ src/tests/parser_tests.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod parser_tests {
-
-    #[test]
-    fn two_m_two_success() {}
+    // TODO: To test:
+    // - [ ] Make a single node ast
+    // - [ ] Make with 2 children
+    // - [ ] Make with 3 children
 }
