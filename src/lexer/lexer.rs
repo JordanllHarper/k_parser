fb5d5f3aafdd42a327be0a488c2130e8d4f25a36
JordanH <jordanh@apadmi.com>
2023-11-11 08:32:32 +0000

    Organise lexer files
    This will separate out util files from the main lexer file to
    be more readable.
    
    This is open to change if I deem this not necessary.

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,6 +1,6 @@
-use crate::shared::token::{Ident, Token};
+use crate::shared::token::Token;
 
-#[derive(Debug, Clone)]
+use super::lexer_utils::read_identifier;
 /// The Lexer for converting a given input of type String to a stream of Tokens.
 ///
 /// Uses the iterator pattern to provide immutability when moving from one token to the next.
@@ -8,60 +8,11 @@ use crate::shared::token::{Ident, Token};
 /// Also comes with a collect method for String -> Vec<Token> convenience!
 ///
 /// Mainly informed by TJ Devries's parser.
+#[derive(Debug, Clone)]
 pub struct Lexer {
     position: usize,
     input: String,
     character: Option<char>,
-}
-
-/// Returns true if it is an identifier character.
-/// E.g. a-zA-Z or _ -> characters that aren't non identifiable and specific to operations such as
-/// + or -.
-fn is_identifier_character(character: char) -> bool {
-    character.is_alphanumeric() || character == '_'
-}
-
-/// Matches an input of some identifier with it's token.
-/// Some if it matches the list of identifiers.
-/// None if it doesn't match.
-fn match_identifier(input: &str) -> Option<Ident> {
-    match input {
-        "fun" => Some(Ident::Fun),
-        "main" => Some(Ident::Main),
-        "val" => Some(Ident::Val),
-        "var" => Some(Ident::Var),
-        _ => None,
-    }
-}
-
-/// iterates through a string looking for a non identifiable character
-/// - usize = the length traversed in the operation
-/// - Option<Ident> = whether an Ident was found
-fn seek(input: &str) -> (usize, Ident) {
-    let split_val_vec: Vec<&str> = input.split(|c| !is_identifier_character(c)).collect();
-
-    let value = match split_val_vec.first() {
-        Some(split) => split,
-        None => return (1, Ident::NonIdentifiable(input.to_string())),
-    };
-
-    let amount_traversed = value.len();
-
-    match match_identifier(value) {
-        Some(i) => return (amount_traversed, i),
-        None => return (amount_traversed, Ident::NonIdentifiable(value.to_string())),
-    };
-}
-
-/// Reads an identifier such as a keyword 'fun' and tokenises
-fn read_identifier(lexer: &Lexer) -> (Lexer, Token) {
-    let (amount_traversed, ident) = seek(&lexer.input.split_at(lexer.position).1);
-
-    let new_lexer = lexer.advance(amount_traversed);
-
-    let token = Token::Identifier(ident);
-
-    (new_lexer, token)
 }
 
 /// Lexer for creating tokens.
