8d8ac7f3e8f951e2765ed9cfd0c2cc4a312094b0
JordanH <jordanh@apadmi.com>
2023-11-13 22:19:03 +0000

    Delete parser tests
    These tests are going to be reworked to the new ASTNode model

--- src/tests/parser_tests.rs
+++ src/tests/parser_tests.rs
@@ -16,58 +16,11 @@ mod parser_tests {
     };
 
     #[test]
-    fn make_single_blank_node() {
-        let tokens: Vec<Token> = vec![];
-        let parser = AstParser::new(tokens);
-        let expected = AstNode::new(
-            Arc::new(None),
-            Token::Identifier(crate::shared::token::Ident::NonIdentifiable(String::from(
-                "",
-            ))),
-        );
-        let (node, _parser) = parser.update_tree();
-
-        assert_eq!(expected.node_token, node.node_token)
-    }
+    fn make_single_blank_node() {}
 
     #[test]
-    fn make_single_node_with_comma() {
-        let tokens: Vec<Token> = vec![Token::Comma];
-        let parser = AstParser::new(tokens);
-        let expected = AstNode::new(Arc::new(None), Token::Comma);
-        let (node, _parser) = parser.update_tree();
-
-        assert_eq!(expected.node_token, node.node_token)
-    }
+    fn make_single_node_with_comma() {}
 
     #[test]
-    fn parent_child() {
-        let tokens: Vec<Token> = vec![Token::Comma, Token::Space];
-        let expected_children = vec![AstNode::new(Arc::new(None), Token::Space)];
-        let parser = AstParser::new(tokens);
-        let expected = AstNode::new(Arc::new(Some(expected_children)), Token::Comma);
-        let (_, parser) = parser.update_tree();
-        let (first_node, _parser) = parser.update_tree(); //tree with 1 child
-        assert_eq!(expected.node_token, first_node.node_token);
-        let expected_child = expected
-            .children
-            .as_ref()
-            .clone()
-            .unwrap()
-            .get(0)
-            .unwrap()
-            .node_token
-            .clone();
-        let actual_child = first_node
-            .children
-            .as_ref()
-            .clone()
-            .unwrap()
-            .get(0)
-            .unwrap()
-            .node_token
-            .clone();
-        assert_eq!(expected_child, actual_child)
-        // let second_node = first_node.children.unwrap();
-    }
+    fn parent_child() {}
 }
