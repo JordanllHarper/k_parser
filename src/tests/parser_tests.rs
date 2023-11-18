b41a1bc707653b85633b6ccf463bfffad4a26f9d
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2023-11-18 18:46:24 +0000

    Write test for putting a single function token in the tree

--- src/tests/parser_tests.rs
+++ src/tests/parser_tests.rs
@@ -1,26 +1,26 @@
 #[cfg(test)]
-mod parser_tests {
-    // TODO: To test:
-    // - [ ] Make a single blank node ast
-    // - [ ] Make a single node ast
-    // - [ ] Make with 2 children
-    // - [ ] Make with 3 children
+pub mod parser_tests {
     use std::sync::Arc;
 
     use crate::{
         parser::{
-            ast_node::AstNode,
+            ast_node::{AstNode, NodeType, ParentSemantics},
             parser::{AstParser, Parser},
         },
-        shared::token::Token,
+        shared::token::*,
     };
 
     #[test]
-    fn make_single_blank_node() {}
-
-    #[test]
-    fn make_single_node_with_comma() {}
-
-    #[test]
-    fn parent_child() {}
+    fn one_token_node() {
+        let test_data = vec![Token::Keyword(Keyword::Fun)];
+        let parser = AstParser::new(test_data);
+        let expected = AstNode::new(NodeType::Parent {
+            semantics: ParentSemantics::Root,
+            children: Arc::new(vec![AstNode::new(NodeType::Child(Token::Keyword(
+                Keyword::Fun,
+            )))]),
+        });
+        let (node, _parser) = parser.update_tree();
+        assert_eq!(expected, node);
+    }
 }
