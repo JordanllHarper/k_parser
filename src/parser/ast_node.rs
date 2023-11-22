0502fb790bc2b517af3e28057306859215c9b518
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2023-11-22 09:39:55 +0000

    Add docs and renames

--- src/parser/ast_node.rs
+++ src/parser/ast_node.rs
@@ -29,10 +29,26 @@ pub enum NodeType {
     Child(Token),                           // ends of the tree
     Parent { children: Arc<Vec<AstNode>> }, // parents that join the end
 }
+
+/// A semantic node of the AST.
+///
+/// Contains a token, the parent and the child of this node.
+///
+/// The parent and children will be None if there is none.
+#[derive(Clone, Debug, PartialEq)]
+pub struct SemanticAstNode {
+    node_type: SemanticNodeType,
+}
+
+/// Semanatic Node type of the non semantic tree.
+///
+/// Parent contains semantics.
+#[derive(Clone, Debug, PartialEq)]
+pub enum SemanticNodeType {
     Child(Token), // ends of the tree
     Parent {
         semantics: ParentSemantics,
-        children: Arc<Vec<AstNode>>,
+        children: Arc<Vec<SemanticAstNode>>,
     }, // parents that join the end
 }
 
@@ -46,8 +62,8 @@ pub enum ParentSemantics {
     Root,
 }
 
-impl AstNode {
-    pub fn new(node_type: NodeType) -> Self {
+impl SemanticAstNode {
+    pub fn new(node_type: SemanticNodeType) -> Self {
         Self { node_type }
     }
 }
